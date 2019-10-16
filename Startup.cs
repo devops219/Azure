1a220d906c3982eafd7845d3c4da91635bb74374
devops219 <51562285+devops219@users.noreply.github.com>
2019-10-16 14:59:11 +0530

    Add files via upload

--- Startup.cs
+++ Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Dileep
+namespace WebApplication1
 {
     public class Startup
     {
